ec2f5b72cd517c17b6fbecfe3b99dc9a2614a6e5
Joshua Cambal <joshuacambal01@gmail.com>
2023-05-04 18:04:56 +0800

    Added Back-end, Database, Front-end Folder.

---
+++ Back-end/useraccount.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigiSort_Box.Back_end
+{
+     class useraccount
+    {
+
+    }
+}
